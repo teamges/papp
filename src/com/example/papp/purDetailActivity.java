4d4e806de8fe5415375994ff84f041b4cd1d0c50
97111701 <97111701@GEK03-05.merletcollege.nl>
2014-10-28 15:58:55 +0100

    dsaf

--- src/com/example/papp/purDetailActivity.java
+++ src/com/example/papp/purDetailActivity.java
@@ -1,12 +1,13 @@
 package com.example.papp;
 
+import android.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
-/** test test lol
+/** test test lol dlksjflkdsajfokldsjalf
  * An activity representing a single pur detail screen. This
  * activity is only used on handset devices. On tablet-size devices,
  * item details are presented side-by-side with a list of items
